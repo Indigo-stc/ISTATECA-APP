98d02456a67606080225d36cae5ae0cc35518ceb
kronnor <kevin.landivar.est@tecazuay.edu.ec>
2023-06-29 18:17:08 -0500

    sugerenciahtml

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-footer',
@@ -6,7 +6,18 @@ import { Component } from "@angular/core";
     styleUrls:['./footer.component.css']
 
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
     autor: any = {nombre: 'Johann', apellido: 'Arizaga'}
 
+    displayStyle = "none";
+  
+  openPopup() {
+    this.displayStyle = "block";
+  }
+  closePopup() {
+    this.displayStyle = "none";
+  }
 }
