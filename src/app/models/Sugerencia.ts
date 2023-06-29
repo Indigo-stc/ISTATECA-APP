88b840da5dae75f85a955fa0207d6222d5efc0ae
kronnor <kevin.landivar.est@tecazuay.edu.ec>
2023-06-29 18:37:28 -0500

    backsugerencia

---
+++ src/app/models/Sugerencia.ts
@@ -0,0 +1,8 @@
+import { Persona } from "./Persona";
+
+export class Sugerencia {
+    id?: number;
+    description?: string;
+    fecha?: Date;
+    idpersona?: Persona;
+}
---
+++ src/app/services/sugerencia.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Sugerencia } from '../models/Sugerencia';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class sugerenciaService{
+
+}
