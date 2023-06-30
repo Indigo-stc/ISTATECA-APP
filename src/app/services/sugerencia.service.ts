b32c96fbd698c214c900aa90a910eab9d59e8c18
kronnor <kevin.landivar.est@tecazuay.edu.ec>
2023-06-29 19:18:19 -0500

    fail

--- src/app/services/sugerencia.service.ts
+++ src/app/services/sugerencia.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Sugerencia } from '../models/Sugerencia';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
   })
 export class sugerenciaService{
 
+  private urlCrearsugerencia:String = 'http://localhost:8080/sugerencia/crear';
+  
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
+  constructor(private http: HttpClient) { }
+
+  /*create(sugerencias: Sugerencia): Observable<Sugerencia> {
+    return this.http.post<Sugerencia>(this.urlCrearsugerencia, sugerencias,{headers:this.httpHeaders})
+
+  }*/
 }
